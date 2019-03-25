934d2f09d2c20eca7d23fdcc2fa0cfd058b3273e
3A <kiennd010498@gmail.com>
2019-03-25 20:52:45 +0700

    Add routing in SPA

---
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../_services/auth.service';
+import { ArlertifyService } from '../_services/arlertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router,
+    private alertify: ArlertifyService) {}
+
+  canActivate(): boolean {
+    if(this.authService.loggedIn()) {
+      return true;
+    }
+    this.alertify.error('You shall not pass!!!');
+    this.router.navigate(['/home']);
+    return false;
+  }
+}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { ArlertifyService } from '../_services/arlertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,8 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: ArlertifyService) { }
+  constructor(public authService: AuthService, private alertify: ArlertifyService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,6 +23,8 @@ export class NavComponent implements OnInit {
       this.alertify.success('Logged in sucessfully');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -31,5 +35,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
